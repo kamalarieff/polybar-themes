307bf3a42e122e1b9d0e0a18eeb099c63e5779ef
Kamal Arieff <kamal.arieff@mudah.my>
2020-09-26 15:18:09 +0800

    [master] Add taskwarrior scripts

--- polybar-2/scripts/task_polybar.sh
+++ polybar-2/scripts/task_polybar.sh
@@ -2,8 +2,25 @@
 
 # https://github.com/dakuten/taskwarrior-polybar
 
+# TODO: Fix active task not showing when change context
+# current_context=`task _get rc.context`
+
+# task context none > /dev/null 2>&1
+
+active_desc=`task rc.verbose: rc.report.active.columns:description rc.report.active.labels:1 limit:1 active`
+active_id=`task rc.verbose: rc.report.active.columns:id rc.report.active.labels:1 limit:1 active`
+
 most_urgent_desc=`task rc.verbose: rc.report.next.columns:description rc.report.next.labels:1 limit:1 next`
 most_urgent_id=`task rc.verbose: rc.report.next.columns:id rc.report.next.labels:1 limit:1 next`
-most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
-echo "$most_urgent_id" > /tmp/tw_polybar_id
-echo "id: $most_urgent_id - $most_urgent_desc"
+# most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
+
+if [ -n "$active_id" ]
+then
+    echo "$active_id" > /tmp/tw_polybar_id
+    echo "(active) id: $active_id - $active_desc"
+else
+    echo "$most_urgent_id" > /tmp/tw_polybar_id
+    echo "id: $most_urgent_id - $most_urgent_desc"
+fi
+
+# task context $current_context > /dev/null 2>&1
